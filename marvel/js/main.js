52215d30c14ab094b3f7026a839a068036b586b4
Maximilian284 <61289731+Maximilian284@users.noreply.github.com>
2021-04-01 17:08:29 +0200

    add something

---
+++ marvel/js/main.js
@@ -0,0 +1,113 @@
+let dict = {
+  "film" : {
+    "Capitan America 1" : "https://drive.google.com/file/d/1acNHDV9fel-E-IE4kB3znJqzUE_SReCy/preview",
+    "Capitan Marvel" : "https://drive.google.com/file/d/1e1tPgp6zwZr-MX-VHqD7RRrhwK4a4YP8/preview",
+    "Iron Man 1" : "https://drive.google.com/file/d/1cyBvOOXvf2BrvEjSR4i3YIqnfyzpz2wr/preview",
+    "Iron Man 2" : "https://drive.google.com/file/d/1cwPrX34QUDytPbYCNqHa6plvR7_7LJrj/preview",
+    "L incredibile Hulk" : "https://drive.google.com/file/d/1dsuwm5AZjrBsJkPiWKmKKtuqaQ4wMoQ_/preview",
+    "Thor 1" : "https://drive.google.com/file/d/1gMo6pv8P2_xLTjqgsnUxhof6qRAds0Sf/preview",
+    "Avengers 1" : "https://drive.google.com/file/d/13LTV87-i4I0RelTIuX2GlchItoKBZQ1O/preview",
+    "Iron Man 3" : "https://drive.google.com/file/d/14I-OjI9qMbO0U-I3n3GiHJj8wnPleLRa/preview",
+    "Thor: the Dark World" : "https://drive.google.com/file/d/1_AMBrzV1AyEbowKmOCs8LTwuHEJQY5Gc/preview",
+    "Capitan America: the Winter Soldier" : "https://drive.google.com/file/d/1UiUqKAsCFVOg7gIzv2mSGpJIjQszMJLS/preview",
+    "Guardiani della Galassia" : "https://drive.google.com/file/d/1cTu5kC8gUsDJT2S2IVM2PpMjgoPWNaHL/preview",
+    "Guardiani della Galassia Vol. 2" : "https://drive.google.com/file/d/1BYiFE0s3i6gpl3wBLzAZFg-kggb-nXZH/preview",
+    "Avengers: Age of Ultron" : "https://drive.google.com/file/d/1xy4WpQCWIGo3k2dm1OBsMYlQFQHmYnKs/preview",
+    "Ant-Man" : "https://drive.google.com/file/d/1k-uo6rhDNkoDyKgjWinZTBz9aIT1SWJo/preview",
+    "Capitan America: Civil War" : "https://drive.google.com/file/d/1L_r77_EUduwzXiB-aU3q4UV5y6sub1D2/preview",
+    "Black Widow" : "",
+    "Spider-Man: Homecoming" : "https://drive.google.com/file/d/1Bxeoi6aRiRyBrHr_9PB9SAgCxb3ldN_S/preview",
+    "Doctor Strange" : "https://drive.google.com/file/d/10VEgO4Fqn0dkw51LxmaD9jHwjCB6N10V/preview",
+    "Black Panther" : "https://drive.google.com/file/d/1v2I4vJguXM7qZ2GRlAWawUQPOeMUBKtp/preview",
+    "Thor: Ragnarok" : "https://drive.google.com/file/d/1OVvweREDl5yK7ZRzvpm8VlSyq2X474PV/preview",
+    "Avengers: Infinity War" : "https://drive.google.com/file/d/1VWJJp-2NfgyZSzctp469vty4ItBXfG6-/preview",
+    "Ant-Man and The Wasp" : "https://drive.google.com/file/d/180dd9CceIVRoRxLuprwBPt8cMF3X94XH/preview",
+    "Avengers: Endgame" : "https://drive.google.com/file/d/1M1Xm2J0xIjiq4JJUxKVydVh5Rlz1ndwf/preview",
+    "Spider-Man: Far From Home" : "https://drive.google.com/file/d/1DXFCCkG_wBUZQS5p59b5rNcWY-KeePd7/preview"
+  }, "serieTv": {
+    "Wandavision" : {
+      "episodio 01x01" : "https://drive.google.com/file/d/1zcmd6K6w_juuhVR81C5PEGo7iQstWn40/preview",
+      "episodio 01x02" : "https://drive.google.com/file/d/1aUrz_zRlDioDnH_b0UWrPPuuFAlBNIRr/preview",
+      "episodio 01x03" : "https://drive.google.com/file/d/15QVyxERkuxgMH9L1hhYYVRE8FUHJPEhq/preview",
+      "episodio 01x04" : "https://drive.google.com/file/d/1i024Q_rldFn5zp9vIm6f87S9zRtiqCmD/preview",
+      "episodio 01x05" : "https://drive.google.com/file/d/1JJZ_H3fH1PvT9sJFYNTHMkXF0Lz6HnWE/preview",
+      "episodio 01x06" : "https://drive.google.com/file/d/1CfMTkxfk445yjSllNOhhdr_wFxhKNPI8/preview",
+      "episodio 01x07" : "https://drive.google.com/file/d/1wjy99fZZcRR0HA0Ulfu4cXnvCvmDcd_5/preview",
+      "episodio 01x08" : "https://drive.google.com/file/d/1Jykb1JfsWR_8R30Flqj6D0OsxYZQhBmI/preview",
+      "episodio 01x09" : "https://drive.google.com/file/d/1H7vc5uH2P5Y2lHneAuwGrwH7AVQVh3jD/preview"
+    },
+    "The_Falcon_and_The_Winter_Soldier" : {
+      "episodio 01x01" : "https://drive.google.com/file/d/15iVRdiBOgQlnnSW1CuqakTORA__M-Y62/preview",
+      "episodio 01x02" : "https://drive.google.com/file/d/1gEuOCy2stfO06qu61ypzjwO6FVz1sZRU/preview",
+      "episodio 01x03" : "",
+      "episodio 01x04" : "",
+      "episodio 01x05" : "",
+      "episodio 01x06" : ""
+    }
+  }
+}
+
+let currentVideo = {
+  "type" : "",
+  "name" : "",
+  "episode" : ""
+} 
+
+function play(type, name, episode=""){
+  open("videoplayer.html")
+
+  currentVideo.type = type
+  currentVideo.name = name
+  currentVideo.episode = episode 
+
+  localStorage.setItem("currentVideo", JSON.stringify(currentVideo))
+}
+
+function loadVideo(){
+  let videoplayer = document.getElementById("videoplayer")
+  let video 
+
+  currentVideo = JSON.parse(localStorage.getItem("currentVideo"))
+
+  if (currentVideo.episode == "") {
+    video = dict[currentVideo.type][currentVideo.name]
+  } else {
+    video = dict[currentVideo.type][currentVideo.name][currentVideo.episode]
+  }
+  
+  videoplayer.setAttribute("src", video)
+}
+
+function setTvSerie(name){
+  open("tv-series.html")
+  localStorage.setItem("tvSerieName", JSON.stringify(name))
+}
+
+function loadTvSerie(){
+  let name = JSON.parse(localStorage.getItem("tvSerieName"))
+  let link = document.createElement("a")
+  let text = document.createTextNode(name)
+  link.appendChild(text)
+
+  let element = document.getElementById("nome-serie")
+  element.appendChild(link)
+
+  link.setAttribute("class", "nav-link")
+
+  name = name.replace(/ /g, "_")
+
+  let list = document.createElement("li")
+
+  for (let i in dict["serieTv"][name]){
+    let list = document.createElement("li")
+    let a = document.createElement("a")
+    let text = document.createTextNode(i)
+    a.appendChild(text)
+    list.appendChild(a)
+
+    let element = document.getElementById("elenco-episodi")
+    element.append(list)
+    let string = `play('serieTv', '${name}', '${i}')`
+    a.setAttribute("onclick", string)
+  }
+}
